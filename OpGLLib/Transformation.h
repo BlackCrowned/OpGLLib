e6e9db0107b7a5166e8bfdebdc575865e895a6d5
Michael Möhrle <mi.moehrle@gmail.com>
2014-11-22 22:28:20 +0100

    Transformation-Klasse hinzugefügt

---
+++ OpGLLib/Transformation.h
@@ -0,0 +1,59 @@
+/*
+ * Transformation.h
+ *
+ *  Created on: 22.11.2014
+ *      Author: Michael
+ */
+
+#ifndef OPGLLIB_TRANSFORMATION_H_
+#define OPGLLIB_TRANSFORMATION_H_
+
+#include <OpGLLib/internal.h>
+#include <OpGLLib/Matrices.h>
+
+enum MultiplicationOrder {
+	SRT = 0x1,
+	RST = 0x2,
+	RTS = 0x4,
+	STR = 0x8,
+	TSR = 0x10,
+	TRS = 0x20,
+};
+
+class Transformation {
+public:
+	Transformation();
+	Transformation(Matrices *matrices);
+	~Transformation();
+
+	void setMatricesHandle(Matrices *newHandle);
+	Matrices *getMatricesHandle();
+	void deleteMatricesHandle();
+
+	void setTransformationMatrix(glm::mat4 transformationMatrix);
+	void resetTransformationMatrix();
+	void updateTransformationMatrix(MultiplicationOrder = SRT);
+	glm::mat4 getTransformationMatrix(bool noPerspectiveTransform = false);
+
+	void translate(glm::vec3 offset);
+	void translateX(gl::GLfloat x);
+	void translateY(gl::GLfloat y);
+	void translateZ(gl::GLfloat z);
+
+	void scale(glm::vec3 scale);
+	void scaleX(gl::GLfloat x);
+	void scaleY(gl::GLfloat y);
+	void scaleZ(gl::GLfloat z);
+
+	void rotate(glm::vec3 rotation);
+	void rotateX(gl::GLfloat x);
+	void rotateY(gl::GLfloat y);
+	void rotateZ(gl::GLfloat z);
+
+private:
+	bool handleInitialized;
+	Matrices *matrices;
+	glm::mat4 transformationMatrix;
+};
+
+#endif /* OPGLLIB_TRANSFORMATION_H_ */
---
+++ src/Transformation.cpp
@@ -0,0 +1,146 @@
+/*
+ * Transformation.cpp
+ *
+ *  Created on: 22.11.2014
+ *      Author: Michael
+ */
+
+#include <OpGLLib/Transformation.h>
+
+using namespace gl;
+using namespace std;
+
+Transformation::Transformation() {
+	transformationMatrix = glm::mat4(1.0f);
+	handleInitialized = true;
+	Transformation::matrices = new Matrices();
+}
+
+Transformation::Transformation(Matrices *matrices) {
+	transformationMatrix = glm::mat4(1.0f);
+	handleInitialized = false;
+	Transformation::matrices = matrices;
+}
+
+Transformation::~Transformation() {
+	if (handleInitialized) {
+		deleteMatricesHandle();
+	}
+}
+
+void Transformation::setMatricesHandle(Matrices *newHandle) {
+	if (handleInitialized) {
+		deleteMatricesHandle();
+	}
+	matrices = newHandle;
+}
+
+Matrices *Transformation::getMatricesHandle() {
+	return matrices;
+}
+
+void Transformation::deleteMatricesHandle() {
+	handleInitialized = false;
+	delete matrices;
+}
+
+void Transformation::setTransformationMatrix(glm::mat4 transformationMatrix) {
+	Transformation::transformationMatrix = transformationMatrix;
+}
+
+void Transformation::resetTransformationMatrix() {
+	transformationMatrix = glm::mat4(1.0f);
+}
+
+void Transformation::updateTransformationMatrix(MultiplicationOrder multiplicationOrder) {
+	switch(multiplicationOrder) {
+	case TRS:
+		transformationMatrix = matrices->getScalingMatrix() * matrices->getRotationMatrix() * matrices->getTranslationMatrix();
+		break;
+	case TSR:
+		transformationMatrix = matrices->getRotationMatrix() * matrices->getScalingMatrix() * matrices->getTranslationMatrix();
+		break;
+	case STR:
+		transformationMatrix = matrices->getRotationMatrix() * matrices->getTranslationMatrix() * matrices->getScalingMatrix();
+		break;
+	case RTS:
+		transformationMatrix = matrices->getScalingMatrix() * matrices->getTranslationMatrix() * matrices->getRotationMatrix();
+		break;
+	case RST:
+		transformationMatrix = matrices->getTranslationMatrix() * matrices->getScalingMatrix() * matrices->getRotationMatrix();
+		break;
+	case SRT:
+		transformationMatrix = matrices->getTranslationMatrix() * matrices->getRotationMatrix() * matrices->getScalingMatrix();
+		break;
+	default:
+		cerr << "Wrong Multiplication-Order submitted: " << multiplicationOrder << endl;
+		transformationMatrix = matrices->getTranslationMatrix() * matrices->getRotationMatrix() * matrices->getScalingMatrix();
+	}
+}
+
+glm::mat4 Transformation::getTransformationMatrix(bool noPerspectiveTransform) {
+	if (noPerspectiveTransform) {
+		return transformationMatrix;
+	}
+	return matrices->getPerspectiveMatrix() * transformationMatrix;
+}
+
+void Transformation::translate(glm::vec3 offset) {
+	matrices->setTranslationMatrix(offset);
+	transformationMatrix = matrices->getTranslationMatrix() * transformationMatrix;
+}
+
+void Transformation::translateX(GLfloat x) {
+	matrices->setTranslationMatrixX(x);
+	transformationMatrix = matrices->getTranslationMatrix() * transformationMatrix;
+}
+
+void Transformation::translateY(GLfloat y) {
+	matrices->setTranslationMatrixY(y);
+	transformationMatrix = matrices->getTranslationMatrix() * transformationMatrix;
+}
+
+void Transformation::translateZ(GLfloat z) {
+	matrices->setTranslationMatrixZ(z);
+	transformationMatrix = matrices->getTranslationMatrix() * transformationMatrix;
+}
+
+void Transformation::scale(glm::vec3 scale) {
+	matrices->setScalingMatrix(scale);
+	transformationMatrix = matrices->getScalingMatrix() * transformationMatrix;
+}
+
+void Transformation::scaleX(GLfloat x) {
+	matrices->setScalingMatrixX(x);
+	transformationMatrix = matrices->getScalingMatrix() * transformationMatrix;
+}
+
+void Transformation::scaleY(GLfloat y) {
+	matrices->setScalingMatrixY(y);
+	transformationMatrix = matrices->getScalingMatrix() * transformationMatrix;
+}
+
+void Transformation::scaleZ(GLfloat z) {
+	matrices->setScalingMatrixZ(z);
+	transformationMatrix = matrices->getScalingMatrix() * transformationMatrix;
+}
+
+void Transformation::rotate(glm::vec3 rotation) {
+	matrices->setRotationMatrix(rotation);
+	transformationMatrix = matrices->getRotationMatrix() * transformationMatrix;
+}
+
+void Transformation::rotateX(GLfloat x) {
+	matrices->setRotationMatrixX(x);
+	transformationMatrix = matrices->getRotationMatrix() * transformationMatrix;
+}
+
+void Transformation::rotateY(GLfloat y) {
+	matrices->setRotationMatrixY(y);
+	transformationMatrix = matrices->getRotationMatrix() * transformationMatrix;
+}
+
+void Transformation::rotateZ(GLfloat z) {
+	matrices->setRotationMatrixZ(z);
+	transformationMatrix = matrices->getRotationMatrix() * transformationMatrix;
+}
