117596342e4c5d6556c047c9f51fe527aff7ccfa
Michael Möhrle <mi.moehrle@gmail.com>
2014-11-24 19:50:07 +0100

    Camera-Klasse hinzugefügt

---
+++ OpGLLib/Camera.h
@@ -0,0 +1,25 @@
+/*
+ * Camera.h
+ *
+ *  Created on: 24.11.2014
+ *      Author: Michael
+ */
+
+#ifndef OPGLLIB_CAMERA_H_
+#define OPGLLIB_CAMERA_H_
+
+#include <OpGLLib/internal.h>
+#include <OpGLLib/Matrices.h>
+
+class Camera : private Matrices{
+public:
+	Camera();
+	~Camera();
+
+private:
+
+};
+
+
+
+#endif /* OPGLLIB_CAMERA_H_ */
---
+++ src/Camera.cpp
@@ -0,0 +1,16 @@
+/*
+ * Camera.cpp
+ *
+ *  Created on: 24.11.2014
+ *      Author: Michael
+ */
+
+#include <OpGLLib/Camera.h>
+
+Camera::Camera() : Matrices(){
+
+};
+
+Camera::~Camera() {
+
+}
