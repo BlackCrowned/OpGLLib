6942ab589561ff413f27580e0a70006936841813
Michael Möhrle <mi.moehrle@gmail.com>
2015-01-29 19:58:23 +0100

    Moved template defintions into .inl file

--- OpGLLib/OpGLLibDef.h
+++ OpGLLib/OpGLLibDef.h
@@ -11,22 +11,14 @@
 namespace OpGLLib {
 template<typename T> struct default_delete {
 	constexpr default_delete() noexcept = default;
-	template<typename U> default_delete(default_delete<U> const&) noexcept {
-
-	}
-	void operator ()(T* ptr) const {
-		delete ptr;
-	}
+	template<typename U> default_delete(default_delete<U> const&) noexcept;
+	void operator ()(T* ptr) const;
 };
 
 template<typename T> struct default_delete<T[]> {
 	constexpr default_delete() noexcept = default;
-	template<typename U> default_delete(default_delete<U[]> const&) noexcept {
-
-	}
-	void operator ()(T* ptr) const {
-		delete[] ptr;
-	}
+	template<typename U> default_delete(default_delete<U[]> const&) noexcept;
+	void operator ()(T* ptr) const;
 };
 
 namespace Types {
@@ -110,47 +102,16 @@ template class LONG<Endianness::BIG_ENDIAN> ;
 }
 
 template<class retType = unsigned char, class dataType = unsigned short> retType bitAccess(int ExtractBits, dataType data, int n,
-		Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN) {
-	if (Endian == Types::Endianness::LITTLE_ENDIAN) {
-		return static_cast<retType>((data & (((1 << ExtractBits) - 1) << (n * ExtractBits))) >> (n * ExtractBits));
-	} else {
-		constexpr dataType sizeofDataType = sizeof(dataType) * 8;
-		return static_cast<retType>((data
-				& (((1 << ExtractBits) - 1) << (sizeofDataType - ExtractBits - n * ExtractBits)))
-						>> (sizeofDataType - ExtractBits - n * ExtractBits));
-	}
-}
+		Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN);
 
 template<class retType = unsigned char, class dataType = unsigned short, int ExtractBits = sizeof(retType)> retType bitAccess(dataType data,
-		int n, Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN) {
-	if (Endian == Types::Endianness::LITTLE_ENDIAN) {
-		constexpr dataType bitmask = (1 << ExtractBits) - 1;
-		return static_cast<retType>((data & (bitmask << (n * ExtractBits))) >> (n * ExtractBits));
-	} else {
-		constexpr dataType bitmask = (1 << ExtractBits) - 1;
-		constexpr dataType BEOffset = sizeof(dataType) * 8 - ExtractBits;
-		return static_cast<retType>((data & (bitmask << (BEOffset - n * ExtractBits))) >> (BEOffset - n * ExtractBits));
-	}
-}
+		int n, Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN);
 
 template<class retType = unsigned char, class dataType = unsigned short, int ExtractBits = sizeof(retType), int n = 0> retType bitAccess(
-		dataType data, Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN) {
-	if (Endian == Types::Endianness::LITTLE_ENDIAN) {
-		constexpr dataType bitmask = (1 << ExtractBits) - 1;
-		constexpr dataType shiftAmount = n * ExtractBits;
-		return static_cast<retType>((data & (bitmask << shiftAmount)) >> shiftAmount);
-	} else {
-		constexpr dataType bitmask = (1 << ExtractBits) - 1;
-		constexpr dataType shiftAmount = sizeof(dataType) * 8 - ExtractBits - n * ExtractBits;
-		return static_cast<retType>((data & (bitmask << shiftAmount)) >> shiftAmount);
-	}
-
-}
+		dataType data, Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN);
 
 template<class retType = unsigned char, class dataType = unsigned short, int ExtractBits = sizeof(retType), int n = 0, dataType data = 0,
-		Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN> constexpr retType bitAccess() {
-	return bitAccess<retType, dataType, Endian>(ExtractBits, data, n);
-}
+		Types::Endianness Endian = Types::Endianness::LITTLE_ENDIAN> constexpr retType bitAccess();
 
 }
 
