9362381f195f821257f42a640a74b8fc3a8d2ba8
Michael Möhrle <mi.moehrle@gmail.com>
2015-06-22 21:38:19 +0200

    Experimental Data Storage Structure

---
+++ OpGLLib/Data.h
@@ -0,0 +1,56 @@
+/*
+ * Data.h
+ *
+ *  Created on: 18.06.2015
+ *      Author: mimoe
+ */
+
+#ifndef OPGLLIB_DATA_H_
+#define OPGLLIB_DATA_H_
+
+#define INCLUDE_GLM
+#define INCLUDE_GLBINDING
+#include <OpGLLib/internal.h>
+
+#include <map>
+#include <memory>
+
+namespace OpGLLib {
+
+class Data {
+public:
+	Data() = delete;
+	~Data() = delete;
+
+	class VertexSettings {
+	public:
+		int vertexCount = 0;
+		int vectorLength = 0;
+		int valueSize = 0;
+		::gl::GLenum valueType;
+	};
+
+	class BufferSettings {
+	public:
+		std::shared_ptr<VertexSettings> vertexSettings;
+	};
+
+	template<class T>static std::shared_ptr<VertexSettings> getVertexSettings(T* addr);
+	static std::shared_ptr<BufferSettings> getBufferSettings(unsigned int buffer);
+
+	template<class T>static void setVertexSettings(T* addr, std::shared_ptr<VertexSettings> vertexSettings);
+	static void setBufferSettings(unsigned int buffer, std::shared_ptr<BufferSettings> bufferSettings);
+
+	template<class T>static void deleteVertexSettings(T* addr);
+	static void deleteBufferSettings(unsigned int buffer);
+
+private:
+	static std::map<void*, std::shared_ptr<VertexSettings>> _vertexSettings;
+	static std::map<unsigned int, std::shared_ptr<BufferSettings>> _bufferSettings;
+};
+
+}
+
+#include <OpGLLib/Data.inl>
+
+#endif /* OPGLLIB_DATA_H_ */
---
+++ src/Data.cpp
@@ -0,0 +1,40 @@
+/*
+ * Data.cpp
+ *
+ *  Created on: 18.06.2015
+ *      Author: mimoe
+ */
+
+#include <OpGLLib/Data.h>
+
+namespace OpGLLib {
+
+std::map<void*, std::shared_ptr<Data::VertexSettings>> Data::_vertexSettings;
+std::map<unsigned int, std::shared_ptr<Data::BufferSettings>> Data::_bufferSettings;
+
+
+void Data::setBufferSettings(unsigned int buffer, std::shared_ptr<BufferSettings> bufferSettings) {
+	//Check for existing bufferSettings with key 'buffer'
+	if (_bufferSettings.count(buffer) > 0) {
+		return;
+	}
+
+	//Store bufferSettings with key 'buffer'
+	_bufferSettings[buffer] = bufferSettings;
+}
+
+auto Data::getBufferSettings(unsigned int buffer) -> std::shared_ptr<BufferSettings>{
+	//Check if bufferSettings with key 'buffer' exists
+	if (_bufferSettings.count(buffer) == 0) {
+		return std::shared_ptr<BufferSettings>();
+	}
+
+	//Return bufferSettings with key 'buffer'
+	return _bufferSettings[buffer];
+}
+
+void Data::deleteBufferSettings(unsigned int buffer) {
+	_bufferSettings.erase(buffer);
+}
+
+} /* namespace OpGLLib */
