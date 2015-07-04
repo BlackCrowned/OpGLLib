4d71562dcbab5acdadc94be6a052390b00671140
Michael Möhrle <mi.moehrle@gmail.com>
2015-07-04 17:24:09 +0200

    Some error checking when loading png's

--- src/ImageLoader.cpp
+++ src/ImageLoader.cpp
@@ -164,7 +164,11 @@ Image<unsigned char> loadPNG(std::string filename) {
 	//Decode png
 	std::vector<unsigned char> data;
 	unsigned int width, height;
-	lodepng::decode(data, width, height, filename);
+	int errorCode = lodepng::decode(data, width, height, filename);
+	if (errorCode) {
+		cerr << "Loading " << filename << " failed with error: " << lodepng_error_text(errorCode) << "." << endl;
+	}
+
 
 	//Return Image
 	return Image<unsigned char>({width, height}, std::move(data));
