2c303f011356c48f2fac43ac50f55707ef56416a
Michael Möhrle <mi.moehrle@gmail.com>
2015-07-26 02:15:46 +0200

    Remove OpGLLib class

--- OpGLLib/OpGLLib.h
+++ OpGLLib/OpGLLib.h
@@ -8,41 +8,10 @@
 #ifndef OPGLLIB_OPGLLIB_H_
 #define OPGLLIB_OPGLLIB_H_
 
-#define INCLUDE_GLM
-#define INCLUDE_GLBINDING
 #include <OpGLLib/internal.h>
 
-#include <OpGLLib/Matrices.h>
-#include <OpGLLib/Perspective.h>
-#include <OpGLLib/Camera.h>
-#include <OpGLLib/Transformation.h>
-#include <OpGLLib/Debug.h>
-#include <OpGLLib/Context.h>
-
-#include <chrono>
-#include <deque>
 
 namespace OpGLLib {
-
-class OpGLLib {
-public:
-	OpGLLib();
-	OpGLLib(glbinding::ContextHandle context);
-
-	void _setContext(glbinding::ContextHandle context);
-//
-//	void enableCulling(::gl::GLenum CullFace = ::gl::GL_BACK, ::gl::GLenum FrontFace = ::gl::GL_CCW);
-//	void disableCulling();
-//	void enableDepthTest(::gl::GLboolean DepthMasc = ::gl::GL_TRUE, ::gl::GLenum DepthFunc = ::gl::GL_LEQUAL);
-//	void disableDepthTest();
-
-	//LoadShaders loadShaders;
-	//Matrices matrices;
-	//Perspective perspective;
-	//Camera camera;
-	//Transformation transformation;
-private:
-};
 
 }
 
--- src/OpGLLib.cpp
+++ src/OpGLLib.cpp
@@ -1,25 +1,7 @@
 #include <OpGLLib/OpGLLib.h>
 
-using namespace std;
-using namespace gl;
-
 namespace OpGLLib {
 
-OpGLLib::OpGLLib() {
-#ifndef RElEASE
-	_debug.enableLogging();
-#endif
-}
-
-OpGLLib::OpGLLib(glbinding::ContextHandle context) {
-	_setContext(context);
-#ifndef RElEASE
-	_debug.enableLogging();
-#endif
-}
-
-void OpGLLib::_setContext(glbinding::ContextHandle context) {
-	glbinding::Binding::useContext(context);
 }
 
 }
