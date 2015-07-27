118e5b5f9d57e75172254274ac9991cdb6aed887
Michael Möhrle <mi.moehrle@gmail.com>
2015-07-27 17:19:27 +0200

    Rewrite ModelLoader using tinyObjLoader to load obj files

--- OpGLLib/ModelLoader.h
+++ OpGLLib/ModelLoader.h
@@ -9,184 +9,78 @@
 #define OPGLLIB_MODELLOADER_H_
 
 #define INCLUDE_GLM
-#define INCLUDE_GLBINDING
 #include <OpGLLib/internal.h>
 
-#include <OpGLLib/FileLoader.h>
+#include <OpGLLib/DefaultDelete.h>
+#include <OpGLLib/Logging.h>
 
-#include <regex>
-#include <map>
+#include <string>
+#include <vector>
+#include <memory>
 
-class ModelLoader;
+#include <tinyObjLoader/tiny_obj_loader.h>
 
-//struct Object {
-//	std::string name = "";
-//	int count = 0;
-//	int lineSmoothing = 0;
-//	std::vector<glm::vec4> vertices;
-//	std::vector<glm::vec2> textureVertices;
-//	std::vector<glm::vec3> normals;
-//	std::vector<glm::uvec3> indicies;
-//	std::vector<glm::uvec3> textureIndicies;
-//	std::vector<glm::uvec3> normalIndicies;
-//};
+namespace OpGLLib {
+namespace gl {
 
-//struct Model {
-//	std::string name = "";
-//	unsigned int id = 0;
-//	int count = 0;
-//	int loaded = false;
-//	std::vector<Object> objects;
-//};
-
-class ObjectBase {
-public:
-	ObjectBase() = default;
-	ObjectBase(ObjectBase const& other) = default;
-	ObjectBase(ObjectBase&& other) = default;
-	virtual ~ObjectBase() = 0;
-
-	ObjectBase& operator=(ObjectBase const& other) = default;
-
-	virtual std::string& getName() = 0;
-	virtual int& getVerticesCount() = 0;
-	virtual int& getLineSmoothing() = 0;
-	virtual std::vector<glm::vec4>& getVertices() = 0;
-	virtual std::vector<glm::vec2>& getTextureVertices() = 0;
-	virtual std::vector<glm::vec3>& getNormals() = 0;
-	virtual std::vector<glm::uvec3>& getIndicies() = 0;
-	virtual gl::GLenum getIndiciesType() = 0;
-	virtual const void* getIndiciesOffset() = 0;
-	virtual std::vector<glm::uvec3>& getTextureIndicies() = 0;
-	virtual std::vector<glm::uvec3>& getNormalIndicies() = 0;
-
-	virtual void setName(std::string) = 0;
-	virtual void setVerticesCount(int) = 0;
-	virtual void setLineSmoothing(int) = 0;
-	virtual void setVertices(std::vector<glm::vec4>) = 0;
-	virtual void setTextureVertices(std::vector<glm::vec2>) = 0;
-	virtual void setNormals(std::vector<glm::vec3>) = 0;
-	virtual void setIndicies(std::vector<glm::uvec3>) = 0;
-	virtual void setIndiciesType(gl::GLenum) = 0;
-	virtual void setIndiciesOffset(const void* ) = 0;
-	virtual void setTextureIndicies(std::vector<glm::uvec3>) = 0;
-	virtual void setNormalIndicies(std::vector<glm::uvec3>) = 0;
+enum class MeshType {
+	OBJ = 0
 };
 
-class ModelBase {
+class Mesh {
 public:
-	ModelBase() = default;
-	ModelBase(ModelBase const& other) = default;
-	ModelBase(ModelBase&& other) = default;
-	virtual ~ModelBase() = 0;
+	Mesh() = default;
+	virtual ~Mesh() = default;
 
-	ModelBase& operator=(ModelBase const& other) = default;
+	virtual std::string& name() = 0;
 
-	virtual std::string& getName() = 0;
-	virtual unsigned int getId() = 0;
-	virtual int& getObjectCount() = 0;
-	virtual int& getLoadedState() = 0;
-	virtual std::shared_ptr<ObjectBase> getObject(int id) = 0;
-
-	virtual void setName(std::string) = 0;
-	virtual void setId(unsigned int) = 0;
-	virtual void setObjectCount(int) = 0;
-	virtual void setLoadedState(int) = 0;
-	virtual void setObject(int, std::shared_ptr<ObjectBase>) = 0;
-
-	virtual void addObject(std::shared_ptr<ObjectBase>) = 0;
+	virtual std::vector<glm::vec3>& vertices() = 0;
+	virtual std::vector<glm::vec3>& normals() = 0;
+	virtual std::vector<glm::vec2>& texCoords() = 0;
+	virtual std::vector<glm::uvec3>& indices() = 0;
 };
 
-class Object: public ObjectBase {
+template<MeshType T> class MeshImpl {
 public:
-	Object() = default;
-	Object(Object const& other) = default;
-	Object(Object&& other) = default;
-	~Object() = default;
+	MeshImpl() = delete;
+	~MeshImpl() = delete;
+};
 
-	Object& operator=(Object const& other) = default;
+template<> class MeshImpl<MeshType::OBJ> : public Mesh {
+public:
+	MeshImpl() = default;
+	virtual ~MeshImpl() = default;
 
-	std::string& getName();
-	int& getVerticesCount();
-	int& getLineSmoothing();
-	std::vector<glm::vec4>& getVertices();
-	std::vector<glm::vec2>& getTextureVertices();
-	std::vector<glm::vec3>& getNormals();
-	std::vector<glm::uvec3>& getIndicies();
-	gl::GLenum getIndiciesType();
-	const void* getIndiciesOffset();
-	std::vector<glm::uvec3>& getTextureIndicies();
-	std::vector<glm::uvec3>& getNormalIndicies();
+	void setData(tinyobj::shape_t shape);
 
-	void setName(std::string);
-	void setVerticesCount(int);
-	void setLineSmoothing(int);
-	void setVertices(std::vector<glm::vec4>);
-	void setTextureVertices(std::vector<glm::vec2>);
-	void setNormals(std::vector<glm::vec3>);
-	void setIndicies(std::vector<glm::uvec3>);
-	void setIndiciesType(gl::GLenum);
-	void setIndiciesOffset(const void* );
-	void setTextureIndicies(std::vector<glm::uvec3>);
-	void setNormalIndicies(std::vector<glm::uvec3>);
+	virtual std::string& name();
+
+	virtual std::vector<glm::vec3>& vertices();
+	virtual std::vector<glm::vec3>& normals();
+	virtual std::vector<glm::vec2>& texCoords();
+	virtual std::vector<glm::uvec3>& indices();
 
 private:
-	std::string name = "";
-	int count = 0;
-	int lineSmoothing = 0;
-	std::vector<glm::vec4> vertices;
-	std::vector<glm::vec2> textureVertices;
-	std::vector<glm::vec3> normals;
-	std::vector<glm::uvec3> indicies;
-	std::vector<glm::uvec3> textureIndicies;
-	std::vector<glm::uvec3> normalIndicies;
+	std::string _name;
+
+	std::vector<glm::vec3> _vertices;
+	std::vector<glm::vec3> _normals;
+	std::vector<glm::vec2> _texCoords;
+	std::vector<glm::uvec3> _indices;
 };
 
-class Model: public ModelBase {
+class ModelLoader {
 public:
-	Model() = default;
-	Model(Model const& other) = default;
-	Model(Model&& other) = default;
-	~Model() = default;
+	ModelLoader() = delete;
+	~ModelLoader() = delete;
 
-	Model& operator=(Model const& other) = default;
+	static std::shared_ptr<Mesh> load(std::string const& model);
 
-	std::string& getName();
-	unsigned int getId();
-	int& getObjectCount();
-	int& getLoadedState();
-	std::shared_ptr<ObjectBase> getObject(int id);
-
-	void setName(std::string);
-	void setId(unsigned int);
-	void setObjectCount(int);
-	void setLoadedState(int);
-	void setObject(int, std::shared_ptr<ObjectBase>);
-
-	void addObject(std::shared_ptr<ObjectBase>);
-private:
-	std::string name = "";
-	unsigned int id = 0;
-	int count = 0;
-	int loaded = false;
-	std::vector<std::shared_ptr<ObjectBase> > objects;
+protected:
+	static std::shared_ptr<MeshImpl<MeshType::OBJ>> loadObj(std::string const& model);
 };
 
-
-class ModelLoader: private OpGLLib::FileLoader {
-public:
-	ModelLoader();
-	ModelLoader(const std::string& name, unsigned int id);
-	~ModelLoader();
-
-	unsigned int reserve(const std::string& name, unsigned int id);
-	unsigned int load(const std::string& name, unsigned int id);
-	unsigned int unload(unsigned int id);
-
-	std::shared_ptr<ModelBase> getModel(unsigned int id);
-private:
-	void parseObj(std::shared_ptr<ModelBase>& model, const std::string& name, unsigned int id);
-	std::map<unsigned int, std::shared_ptr<ModelBase> > models;
-};
+}
+}
 
 #endif /* OPGLLIB_MODELLOADER_H_ */
--- src/ModelLoader.cpp
+++ src/ModelLoader.cpp
@@ -7,331 +7,108 @@
 
 #include <OpGLLib/ModelLoader.h>
 
-using namespace std;
+namespace OpGLLib {
+namespace gl {
 
-ObjectBase::~ObjectBase() {
+void MeshImpl<MeshType::OBJ>::setData(tinyobj::shape_t shape) {
+	//Set name
+	_name = shape.name;
 
-}
+	//Reserve space
+	_vertices.resize(shape.mesh.positions.size() / 3);
+	_normals.resize(shape.mesh.normals.size() / 3);
+	_texCoords.resize(shape.mesh.texcoords.size() / 2);
+	_indices.resize(shape.mesh.indices.size() / 3);
 
-ModelBase::~ModelBase() {
+	/*Manually copy data*/
+	//Declare required iterators
+	std::vector<float>::iterator floatIt;
+	std::vector<unsigned int>::iterator uintIt;
+	std::vector<glm::vec3>::iterator vec3It;
+	std::vector<glm::vec2>::iterator vec2It;
+	std::vector<glm::uvec3>::iterator uvec3It;
 
-}
-
-std::string& Object::getName() {
-	return name;
-}
-int& Object::getVerticesCount() {
-	return count;
-}
-int& Object::getLineSmoothing() {
-	return lineSmoothing;
-}
-std::vector<glm::vec4>& Object::getVertices() {
-	return vertices;
-}
-std::vector<glm::vec2>& Object::getTextureVertices() {
-	return textureVertices;
-}
-std::vector<glm::vec3>& Object::getNormals() {
-	return normals;
-}
-std::vector<glm::uvec3>& Object::getIndicies() {
-	return indicies;
-}
-gl::GLenum Object::getIndiciesType() {
-	return gl::GL_UNSIGNED_INT;
-}
-const void* Object::getIndiciesOffset() {
-	return static_cast<const void*>(0);
-}
-std::vector<glm::uvec3>& Object::getTextureIndicies() {
-	return textureIndicies;
-}
-std::vector<glm::uvec3>& Object::getNormalIndicies() {
-	return normalIndicies;
-}
-
-void Object::setName(std::string name) {
-	Object::name = std::move(name);
-}
-void Object::setVerticesCount(int count) {
-	Object::count = count;
-}
-void Object::setLineSmoothing(int lineSmoothing) {
-	Object::lineSmoothing = lineSmoothing;
-}
-void Object::setVertices(std::vector<glm::vec4> vertices) {
-	Object::vertices = std::move(vertices);
-}
-void Object::setTextureVertices(std::vector<glm::vec2> textureVertices) {
-	Object::textureVertices = std::move(textureVertices);
-}
-void Object::setNormals(std::vector<glm::vec3> normals) {
-	Object::normals = std::move(normals);
-}
-void Object::setIndicies(std::vector<glm::uvec3> indicies) {
-	Object::indicies = std::move(indicies);
-}
-void Object::setIndiciesType(gl::GLenum indiciesType) {
-	//HardCoded
-}
-void Object::setIndiciesOffset(const void* indiciesOffset) {
-	//HardCoded
-}
-void Object::setTextureIndicies(std::vector<glm::uvec3> textureIndicies) {
-	Object::textureIndicies = std::move(textureIndicies);
-}
-void Object::setNormalIndicies(std::vector<glm::uvec3> normalIndicies) {
-	Object::normalIndicies = std::move(normalIndicies);
-}
-
-std::string& Model::getName() {
-	return name;
-}
-unsigned int Model::getId() {
-	return id;
-}
-int& Model::getObjectCount() {
-	return count;
-}
-int& Model::getLoadedState() {
-	return loaded;
-}
-std::shared_ptr<ObjectBase> Model::getObject(int id) {
-	return objects[id];
-}
-
-void Model::setName(std::string name) {
-	Model::name = std::move(name);
-}
-void Model::setId(unsigned int id) {
-	Model::id = id;
-}
-void Model::setObjectCount(int count) {
-	Model::count = count;
-}
-void Model::setLoadedState(int loadedState) {
-	Model::loaded = loadedState;
-}
-void Model::setObject(int id, std::shared_ptr<ObjectBase> object) {
-	objects[id] = object;
-}
-
-void Model::addObject(std::shared_ptr<ObjectBase> object) {
-	objects.push_back(object);
-}
-
-ModelLoader::ModelLoader() :
-		FileLoader() {
-
-}
-
-ModelLoader::ModelLoader(const string& name, unsigned int id) :
-		FileLoader(name) {
-	load(name, id);
-}
-
-ModelLoader::~ModelLoader() {
-
-}
-
-unsigned int ModelLoader::reserve(const string& name, unsigned int id) {
-	if (open(name) == NULL) {
-		return -1;
+	//Vertices
+	floatIt = shape.mesh.positions.begin();
+	vec3It = _vertices.begin();
+	while (floatIt != shape.mesh.positions.end() && vec3It != _vertices.end()) {
+		*vec3It = glm::vec3(*floatIt, *(floatIt + 1), *(floatIt + 2));
+		vec3It++;
+		floatIt += 3;
 	}
-	close();
-	std::shared_ptr<ModelBase>& model = models[id];
-	if (!model) {
-		model.reset(new Model, OpGLLib::default_delete<ModelBase>());
-		model->setName(name);
-		model->setId(id);
-		model->setLoadedState(false);
+	//Normals
+	floatIt = shape.mesh.normals.begin();
+	vec3It = _normals.begin();
+	while (floatIt != shape.mesh.normals.end() && vec3It != _normals.end()) {
+		*vec3It = glm::vec3(*floatIt, *(floatIt + 1), *(floatIt + 2));
+		vec3It++;
+		floatIt += 3;
 	}
-	return id;
+	//TexCoords
+	floatIt = shape.mesh.texcoords.begin();
+	vec2It = _texCoords.begin();
+	while (floatIt != shape.mesh.texcoords.end() && vec2It != _texCoords.end()) {
+		*vec2It = glm::vec2(*floatIt, *(floatIt + 1));
+		vec2It++;
+		floatIt += 2;
+	}
+	//Indices
+	uintIt = shape.mesh.indices.begin();
+	uvec3It = _indices.begin();
+	while (uintIt != shape.mesh.indices.end() && uvec3It != _indices.end()) {
+		*uvec3It = glm::vec3(*uintIt, *(uintIt + 1), *(uintIt + 2));
+		uvec3It++;
+		uintIt += 3;
+	}
 }
 
-unsigned int ModelLoader::load(const string& name, unsigned int id) {
-	std::shared_ptr<ModelBase>& model = models[id];
-	if (!model) {
-		model.reset(new Model, OpGLLib::default_delete<ModelBase>());
+std::string& MeshImpl<MeshType::OBJ>::name() {
+	return _name;
+}
+
+std::vector<glm::vec3>& MeshImpl<MeshType::OBJ>::vertices() {
+	return _vertices;
+}
+
+std::vector<glm::vec3>& MeshImpl<MeshType::OBJ>::normals() {
+	return _normals;
+}
+
+std::vector<glm::vec2>& MeshImpl<MeshType::OBJ>::texCoords() {
+	return _texCoords;
+}
+
+std::vector<glm::uvec3>& MeshImpl<MeshType::OBJ>::indices() {
+	return _indices;
+}
+
+std::shared_ptr<Mesh> ModelLoader::load(std::string const& model) {
+	std::shared_ptr<Mesh> mesh;
+	std::string fileType = model.substr(model.find_last_of('.') + 1);
+	if (fileType == "obj") {
+		mesh = loadObj(model);
+	} else {
+		LOG("Unable to parse: " + model, LoggingLevel::recoverableError);
 	}
-	if (open(name) == NULL) {
-		return -1;
-	}
-	if (getFileType() == "obj") {
-		//--> Parse obj files:
-		parseObj(model, name, id);
-	}
-	else {
-		cerr << "Unknown fileType: " << getFileType() << endl;
-		close();
-		return -2;
+	return mesh;
+}
+
+std::shared_ptr<MeshImpl<MeshType::OBJ>> ModelLoader::loadObj(std::string const& model) {
+	std::shared_ptr<MeshImpl<MeshType::OBJ>> mesh(new MeshImpl<MeshType::OBJ>(), OpGLLib::default_delete<MeshImpl<MeshType::OBJ>>());
+	std::vector<tinyobj::material_t> materials;
+	std::vector<tinyobj::shape_t> shapes;
+
+	std::string err = tinyobj::LoadObj(shapes, materials, model.c_str());
+
+	if (!err.empty()) {
+		LOG(err, OpGLLib::LoggingLevel::recoverableError);
+		return mesh;
 	}
 
-	close();
-	model->setLoadedState(true);
-	return id;
+	mesh->setData(shapes[0]);
+
+	return mesh;
 }
 
-unsigned int ModelLoader::unload(unsigned int id) {
-	models.erase(id);
-	return id;
 }
-
-std::shared_ptr<ModelBase> ModelLoader::getModel(unsigned int id) {
-	if (!models[id]->getLoadedState()) {
-		load(models[id]->getName(), id);
-	}
-	return models[id];
-}
-
-void ModelLoader::parseObj(std::shared_ptr<ModelBase>& model, const string& name, unsigned int id) {
-	model->setName(name);
-	model->setId(id);
-
-	string line;
-	while(!getFile()->eof()) {
-		getline(*getFile(), line);
-		for (unsigned int i = 0; i < line.length(); i++) {
-			stringstream ss;
-			int x, xT, xN, y, yT, yN, z, zT, zN;
-			switch(line[i]) {
-			//Comment
-			case '#':
-				i = line.length();
-				break;
-			//TODO
-			case 'm':
-				i = line.length();
-				break;
-			//Object Name
-			case 'o':
-				//New Object
-				model->getObjectCount()++;
-				model->addObject(std::shared_ptr<ObjectBase>(new Object, OpGLLib::default_delete<ObjectBase>()));
-				model->getObject(model->getObjectCount() - 1)->setName(line.substr(i + 1));
-				i = line.length();
-				break;
-			//Vertex
-			case 'v':
-				if (model->getObjectCount() == 0) {
-					model->getObjectCount()++;
-					model->addObject(std::shared_ptr<ObjectBase>(new Object, OpGLLib::default_delete<ObjectBase>()));
-				}
-				if (line[i + 1] == ' ') {
-					float x, y, z, w = 1.0f;
-					ss.str(line.substr(i + 1));
-					ss >> x >> y >> z >> w;
-					//model.objects[model.count - 1].count++;
-					//model.objects[model.count - 1].vertices.push_back(glm::vec4(x, y, z, w));
-					model->getObject(model->getObjectCount() - 1)->getVerticesCount()++;
-					model->getObject(model->getObjectCount() - 1)->getVertices().push_back(glm::vec4(x, y, z, w));
-				}
-				else if (line[i + 1] == 't') {
-					float x, y;
-					ss.str(line.substr(i + 2));
-					ss >> x >> y;
-					//model.objects[model.count - 1].textureVertices.push_back(glm::vec2(x, y));
-					model->getObject(model->getObjectCount() - 1)->getTextureVertices().push_back(glm::vec2(x, y));
-				}
-				else if (line[i + 1] == 'n') {
-					float x, y, z;
-					ss.str(line.substr(i + 2));
-					ss >> x >> y >> z;
-					//model.objects[model.count - 1].normals.push_back(glm::vec3(x, y, z));
-					model->getObject(model->getObjectCount() - 1)->getNormals().push_back(glm::vec3(x, y, z));
-				}
-				i = line.length();
-				break;
-				//Edge smoothing
-			case 's':
-				if (model->getObjectCount() == 0) {
-					model->getObjectCount()++;
-					model->addObject(std::shared_ptr<ObjectBase>(new Object, OpGLLib::default_delete<ObjectBase>()));
-				}
-				x = 0;
-				ss.str(line.substr(i + 1));
-				ss >> x;
-				//model.objects[model.count - 1].lineSmoothing = x;
-				model->getObject(model->getObjectCount() - 1)->setLineSmoothing(x);
-				i = line.length();
-				break;
-			case 'f':
-				if (model->getObjectCount() == 0) {
-					model->getObjectCount()++;
-					model->addObject(std::shared_ptr<ObjectBase>(new Object, OpGLLib::default_delete<ObjectBase>()));
-				}
-				string str = line.substr(i + 1);
-				regex pattern("\\d+\\s\\d+\\s\\d+");
-				smatch match;
-				if (regex_search(str, match, pattern)) {
-					ss.str(match[0].str());
-					ss >> x >> y >> z;
-					//model.objects[model.count - 1].indicies.push_back(glm::uvec3(--x, --y, --z));
-					model->getObject(model->getObjectCount() - 1)->getIndicies().push_back(glm::uvec3(--x, --y, --z));
-				}
-				else if (regex_search(str, match, pattern.assign("\\d+/\\d+\\s\\d+/\\d+\\s\\d+/\\d+"))) {
-					ss.str(match[0].str());
-					ss >> x;
-					ss.get();
-					ss >> xT;
-					ss >> y;
-					ss.get();
-					ss >> yT;
-					ss >> z;
-					ss.get();
-					ss >> zT;
-					//model.objects[model.count - 1].indicies.push_back(glm::uvec3(--x, --y, --z));
-					//model.objects[model.count - 1].textureIndicies.push_back(glm::uvec3(--xT, --yT, --zT));
-					model->getObject(model->getObjectCount() - 1)->getIndicies().push_back(glm::uvec3(--x, --y, --z));
-					model->getObject(model->getObjectCount() - 1)->getTextureIndicies().push_back(glm::uvec3(--xT, --yT, --zT));
-				}
-				else if (regex_search(str, match, pattern.assign("\\d+//\\d+\\s\\d+//\\d+\\s\\d+//\\d+"))) {
-					ss.str(match[0].str());
-					ss >> x;
-					ss.get();
-					ss.get();
-					ss >> xN;
-					ss >> y;
-					ss.get();
-					ss.get();
-					ss >> yN;
-					ss >> z;
-					ss.get();
-					ss.get();
-					ss >> zN;
-					//model.objects[model.count - 1].indicies.push_back(glm::uvec3(--x, --y, --z));
-					//model.objects[model.count - 1].normalIndicies.push_back(glm::uvec3(--xN, --yN, --zN));
-					model->getObject(model->getObjectCount() - 1)->getIndicies().push_back(glm::uvec3(--x, --y, --z));
-					model->getObject(model->getObjectCount() - 1)->getNormalIndicies().push_back(glm::uvec3(--xN, --yN, --zN));
-				}
-				else if (regex_search(str, match, pattern.assign("\\d+/\\d+/\\d+\\s\\d+/\\d+/\\d+\\s\\d+/\\d+/\\d+"))) {
-					ss.str(match[0].str());
-					ss >> x;
-					ss.get();
-					ss >> xT;
-					ss.get();
-					ss >> xN;
-					ss >> y;
-					ss.get();
-					ss >> yT;
-					ss.get();
-					ss >> yN;
-					ss >> z;
-					ss.get();
-					ss >> zT;
-					ss.get();
-					ss >> zN;
-					//model.objects[model.count - 1].indicies.push_back(glm::uvec3(--x, --y, --z));
-					//model.objects[model.count - 1].textureIndicies.push_back(glm::uvec3(--xT, --yT, --zT));
-					//model.objects[model.count - 1].normalIndicies.push_back(glm::uvec3(--xN, --yN, --zN));
-					model->getObject(model->getObjectCount() - 1)->getIndicies().push_back(glm::uvec3(--x, --y, --z));
-					model->getObject(model->getObjectCount() - 1)->getTextureIndicies().push_back(glm::uvec3(--xT, --yT, --zT));
-					model->getObject(model->getObjectCount() - 1)->getNormalIndicies().push_back(glm::uvec3(--xN, --yN, --zN));
-				}
-				i = line.length();
-			}
-		}
-	}
-
 }
