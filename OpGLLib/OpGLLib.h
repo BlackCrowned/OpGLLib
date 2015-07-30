c1f22eb1f662548e2d3180bde21f559c5ab8e656
Michael Möhrle <mi.moehrle@gmail.com>
2015-07-30 21:12:44 +0200

    Add OpGLLibBase class to enable inheritance of default observers
    Will later allow class restricted ServiceLocators

--- OpGLLib/OpGLLib.h
+++ OpGLLib/OpGLLib.h
@@ -10,8 +10,23 @@
 
 #include <OpGLLib/internal.h>
 
+#include <OpGLLib/Logging.h>
 
 namespace OpGLLib {
+
+class OpGLLibBase {
+public:
+	OpGLLibBase();
+	OpGLLibBase(OpGLLibBase const* pointer);
+
+	void init();
+
+	void setLogger(Logging&& logger);
+	std::shared_ptr<Logging> getLogger();
+
+private:
+	std::shared_ptr<Logging> _logger;
+};
 
 }
 
--- src/OpGLLib.cpp
+++ src/OpGLLib.cpp
@@ -2,4 +2,26 @@
 
 namespace OpGLLib {
 
+OpGLLibBase::OpGLLibBase() :
+		_logger(new Logging(LoggingLevel::debug)) {
+
+}
+
+OpGLLibBase::OpGLLibBase(OpGLLibBase const* pointer) :
+		_logger(pointer->_logger) {
+
+}
+
+void OpGLLibBase::init() {
+
+}
+
+void OpGLLibBase::setLogger(Logging&& logger) {
+	_logger.reset(new Logging(std::forward<Logging>(logger)));
+}
+
+std::shared_ptr<Logging> OpGLLibBase::getLogger(){
+	return _logger;
+}
+
 }
