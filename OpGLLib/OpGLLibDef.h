89a8c1f98580b024e8641c60f9c895417643ae6b
Michael Möhrle <mi.moehrle@gmail.com>
2015-12-13 19:02:58 +0100

    Remove unused WORD, DWORD, LONG classes

--- OpGLLib/OpGLLibDef.h
+++ OpGLLib/OpGLLibDef.h
@@ -14,79 +14,7 @@ namespace Types {
 enum class Endianness {
 	LITTLE_ENDIAN = 0x1, BIG_ENDIAN = 0x2
 };
-typedef unsigned char BYTE;
 
-template<Endianness Type = Endianness::LITTLE_ENDIAN> struct WORD {
-	typedef unsigned short dataType;
-
-	WORD();
-	WORD(char* offset);
-	WORD(dataType data);
-	template<Endianness U> WORD(WORD<U> const& w);
-
-	WORD<Type>& operator =(char* offset);
-	WORD<Type>& operator =(dataType data);
-
-	dataType& operator *();
-	dataType& operator ->();
-	BYTE operator [](int i);
-
-	dataType data;
-};
-template<> WORD<Endianness::BIG_ENDIAN>::WORD(char* offset);
-template<> WORD<Endianness::BIG_ENDIAN>& WORD<Endianness::BIG_ENDIAN>::operator =(char* offset);
-template<> BYTE WORD<Endianness::BIG_ENDIAN>::operator [](int i);
-
-template<Endianness Type = Endianness::LITTLE_ENDIAN> struct DWORD {
-	typedef unsigned long int dataType;
-
-	DWORD();
-	DWORD(char *offset);
-	DWORD(dataType data);
-	template<Endianness U> DWORD(DWORD<U> const& w);
-
-	DWORD<Type>& operator =(char* offset);
-	DWORD<Type>& operator =(dataType data);
-
-	dataType& operator *();
-	dataType& operator ->();
-	BYTE operator [](int i);
-
-	dataType data;
-};
-
-template<> DWORD<Endianness::BIG_ENDIAN>::DWORD(char* offset);
-template<> DWORD<Endianness::BIG_ENDIAN>& DWORD<Endianness::BIG_ENDIAN>::operator =(char* offset);
-template<> BYTE DWORD<Endianness::BIG_ENDIAN>::operator [](int i);
-
-template<Endianness Type = Endianness::LITTLE_ENDIAN> struct LONG {
-	typedef long int dataType;
-
-	LONG();
-	LONG(char *offset);
-	LONG(dataType data);
-	template<Endianness U> LONG(LONG<U> const& w);
-
-	LONG<Type>& operator =(char* offset);
-	LONG<Type>& operator =(dataType data);
-	dataType& operator *();
-
-	dataType& operator ->();
-	BYTE operator [](int i);
-
-	dataType data;
-};
-
-template<> LONG<Endianness::BIG_ENDIAN>::LONG(char* offset);
-template<> LONG<Endianness::BIG_ENDIAN>& LONG<Endianness::BIG_ENDIAN>::operator =(char* offset);
-template<> BYTE LONG<Endianness::BIG_ENDIAN>::operator [](int);
-
-template class WORD<Endianness::LITTLE_ENDIAN> ;
-template class WORD<Endianness::BIG_ENDIAN> ;
-template class DWORD<Endianness::LITTLE_ENDIAN> ;
-template class DWORD<Endianness::BIG_ENDIAN> ;
-template class LONG<Endianness::LITTLE_ENDIAN> ;
-template class LONG<Endianness::BIG_ENDIAN> ;
 }
 
 template<class retType = unsigned char, class dataType = unsigned short> retType bitAccess(int ExtractBits, dataType data, int n,
